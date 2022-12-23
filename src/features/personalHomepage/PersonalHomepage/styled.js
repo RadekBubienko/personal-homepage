2b49e8f4ec1f623e5b501098e0e198cb91dd2ae9
Radek Bubienko <radek@bubienko.pl>
2022-12-23 23:07:05 +0100

    changed padding in main container

--- src/features/personalHomepage/PersonalHomepage/styled.js
+++ src/features/personalHomepage/PersonalHomepage/styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     max-width: 1248px;
-    padding: 16px;
+    padding: 20px;
     margin: auto;
 
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
-      padding: 8px;
+      padding: 16px;
   }
 `;
 
