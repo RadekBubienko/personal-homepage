67cb5f70b01dea83277791e4ee48af3844dc6c20
Radek Bubienko <radek@bubienko.pl>
2023-01-15 22:38:25 +0100

    deleted export from selectThemeState

--- src/common/themeSlice.js
+++ src/common/themeSlice.js
@@ -15,7 +15,7 @@ const themeSlice = createSlice({
 
 export const { toggleTheme } = themeSlice.actions;
 
-export const selectThemeState = (state) => state.theme;
+const selectThemeState = (state) => state.theme;
 
 export const selectIsDarkTheme = (state) => selectThemeState(state).isDarkTheme;
 
