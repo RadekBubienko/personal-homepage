667cf554e1e6ebaebb42887af719ab7fb1f45642
Radek Bubienko <radek@bubienko.pl>
2023-01-16 20:53:58 +0100

    added themeSaga to saga.js

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { personalHomepageSaga } from "../features/personalHomepage/personalHomepageSaga";
+import { themeSaga } from "../common/themeSaga";
 
 export default function* saga() {
-  yield all([personalHomepageSaga()]);
+  yield all([personalHomepageSaga(), themeSaga()]);
 }
